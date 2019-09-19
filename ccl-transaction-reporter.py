8f267662c8185c85f8043fbd20155c751bb913ce
CK <chris.keith@gmail.com>
2019-09-19 11:08:27 -0700

    Correct number of records read from stripe data file.

--- ccl-transaction-reporter.py
+++ ccl-transaction-reporter.py
@@ -9,14 +9,34 @@ def log(message):
     print(str(datetime.datetime.now()) + '\t'+ script_name + ': ' + message)
 
 class Reporter:
-    def read_from_stream_into_dict(self, file_name, key_column_name):
+    def stripe_date(self, datetime_str):
+        date_str, time_str = datetime_str.split(' ')
+        mon, day, year = date_str.split('/')
+        hour, minute = time_str.split(':')
+        return datetime.datetime(int('20' + year), int(mon), int(day), int(hour), int(minute), 0)
+
+    def handle_stripe(self, dict, key_column_name, record):
+        junk, email = record['Customer Description'].split('|')
+        # - compare timestamps, keep latest payment record.
+        if dict.get(email.strip()):
+            current_date = self.stripe_date(dict[email.strip()]['Created (UTC)'])
+            new_date = self.stripe_date(record['Created (UTC)'])
+            if new_date > current_date:
+                dict[email.strip()] = record
+        else:
+            dict[email.strip()] = record
+
+    def handle_members(self, dict, key_column_name, record):
+        dict[record[key_column_name]] = record
+
+    def read_from_stream_into_dict(self, file_name, key_column_name, dict_processing_funct):
         dict = {}
         fieldnames = None
         with open(file_name, 'r', newline='') as infile:
             reader = csv.DictReader(infile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
             fieldnames = reader.fieldnames
             for record in reader:
-                dict[record[key_column_name]] = record
+                dict_processing_funct(dict, key_column_name, record)
         log(str("{: >4d}".format(len(dict))) + ' records read from ' + file_name)
         return fieldnames, dict 
 
@@ -35,8 +55,10 @@ class Reporter:
         return dict_record
 
     def main(self):
-        stripe_fieldnames, stripe_dict_records = self.read_from_stream_into_dict('STRIPE_unified_payments.csv', 'Customer Email')
-        gsheets_fieldnames, gsheets_dict_records = self.read_from_stream('CCL Members Master List - Current Member List', 'Email')
+        stripe_fieldnames, stripe_dict_records = self.read_from_stream_into_dict(
+            'STRIPE_unified_payments.csv', 'Customer Email', self.handle_stripe)
+        gsheets_fieldnames, gsheets_dict_records = self.read_from_stream_into_dict(
+            'CCL Members Master List - Current Member List.csv', 'Email', self.handle_members)
         array_records = []
         for r in stripe_dict_records.values():
             array_records.append(self.to_array(r))
